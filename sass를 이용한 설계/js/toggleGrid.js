72210ab7449844649586b5be047f318fa8cac44e
sangwonle <yacro@hanmail.net>
2017-05-29 09:48:21 +0900

    Add files via upload

---
+++ sass를 이용한 설계/js/toggleGrid.js
@@ -0,0 +1,12 @@
+/*! togglegrid.js @2017 sangwon */ 
+var doc = document;
+var body = doc.body;
+
+doc.addEventListener('keyup', toggleGrid);
+
+function toggleGrid(e) {
+    if (e.keyCode === 71 && e.shiftKey) {
+        body.classList.toggle('show-grid');
+    }
+}
+
